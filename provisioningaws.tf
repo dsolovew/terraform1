4d038a36e130ac66c7d30e6b0504e657b52cd88b
dsolovew <dsolovew@gmail.com>
2020-12-20 19:13:55 +0300

    pem

--- provisioningaws.tf
+++ provisioningaws.tf
@@ -49,5 +49,5 @@ sudo apt update && sudo apt install nginx -y
 
 resource "aws_key_pair" "ssh-key" {
   key_name = "ssh-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file("~/.ssh/ssh-key.pem")
 }
