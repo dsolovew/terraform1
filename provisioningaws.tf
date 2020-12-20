f5ff6ee42314949e909d817ecd2735c82351598f
dsolovew <dsolovew@gmail.com>
2020-12-20 10:08:01 +0300

    nginx1

--- provisioningaws.tf
+++ provisioningaws.tf
@@ -5,6 +5,7 @@ provider "aws" {
  resource "aws_security_group" "web_ssh" {
    name = "web_ssh"
    description = "Web security group"
+   count = 1
 
    ingress  {
      description = "inbound web"
@@ -34,7 +35,8 @@ provider "aws" {
  resource "aws_instance" "nginx" {
    ami = "ami-0dd9f0e7df0f0a138"
    instance_type = "t3.micro"
-   vpc_security_group_ids = [ "aws_security_group.web_ssh.id" ]
+   vpc_security_group  = [ ${"aws_security_group.web_ssh.name"} ]
+   user_data = "${file("nginx_provisioning.sh")}"
    count = 1
    tags = {
      Name = "Nginx"
