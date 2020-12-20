12c799e4e56f861ba77923977081403567836471
dsolovew <dsolovew@gmail.com>
2020-12-20 20:02:31 +0300

    pub6

--- provisioningaws.tf
+++ provisioningaws.tf
@@ -47,7 +47,23 @@ sudo apt update && sudo apt install nginx -y
    }
  }
 
+  resource "aws_intance" "nginx2" {
+    ami = "ami-09558250a3419e7d0"
+    instance_type = "t3.micro"
+    vpc_security_group_ids  = [aws_security_group.webssh.id]
+    key_name = "ssh-key"
+    user_data = <<EOF
+!# /bin/bash
+sudo apt update && sudo apt install nginx -y
+  EOF
+ }
+    count = 1
+    tags = {
+     Name = "nginx2"
+     Environment = "Test"
+   }
+
 resource "aws_key_pair" "ssh-key" {
   key_name = "ssh-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC53chIerJq/+q6dXOSPjZyEQZvGTTOeIwzvY4UFQxFVEx6l4RaiO+ibSmTJSCVqPhYnHQCxGiMWkbm9SvLdOHe/GZuB4yIULRqyWNnn2G9iGxOYM2z6MX0SAa5zW1aRdKzxtTE9QetpBAATewvARC4wrxH8gl8oRRpdtDtD32xQMZ1rQ8x2nBD985yL5bvnwIzo+WgsWUpzNEbo/hwgpZPmX+bPRHFB5iaYdluDImQIL3QxmkfAuy63YWcx69FqNoDVa32Blv59+uG740Fqwy2U1bP7bFDBlr8tzjiN3ZORSnmF7yGRPYraF3SPk7nyEZHCBnHqtHYkSeeokQerWbL
 }
